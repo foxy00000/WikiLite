9ae24f1b66a5f663ec86c321016fad5d85965882
foxy00000 <123758672+foxy00000@users.noreply.github.com>
2025-11-29 15:29:52 +0000

    Deploying to gh-pages from @ foxy00000/WikiLite@d5bb3872a79fdd4e2c9287fa367c251564f2ec58 🚀

--- custom/custom.js
+++ custom/custom.js
@@ -1,8 +1,17 @@
-// function definitions
+// Intial calling
+document.getElementById('root').innerHTML += `
+<div id="loading-screen">
+    <div class="spinner"></div>
+    <div class="loading-text">Loading your notes…</div>
+</div>`;
 
-// function() { ... }
+// function definitions
+function removeSpinningWheel() {
+    const loader = document.getElementById('loading-screen');
+    if (loader) loader.style.display = 'none';
+}
 
 // All functions defined above need to be loaded here
 document.addEventListener("DOMContentLoaded", function() {
-    // function callings
+    removeSpinningWheel();
 });
